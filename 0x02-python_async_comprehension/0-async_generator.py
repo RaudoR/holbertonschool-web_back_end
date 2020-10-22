b19e45951a649f4cded8c5edfddf2a5ccd83aaf9
raudor <rivaspaulino@outlook.com>
2020-10-21 20:08:04 -0700

    0-async_generator.py

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Write a coroutine called async_generator that takes no arguments. """
+import asyncio
+from typing import Generator
+import random
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """ Async Generator """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
