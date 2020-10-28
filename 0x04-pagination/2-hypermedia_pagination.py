44fb48db397067d5e1843fb20bc35ec29cf5c186
raudor <rivaspaulino@outlook.com>
2020-10-28 16:06:15 -0700

    task 2

--- 0x04-pagination/2-hypermedia_pagination.py
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -6,9 +6,9 @@ from typing import List, Dict, Any
 
 
 def index_range(page, page_size):
-    """ return a tuple of size two containing a start index and an end index
-        corresponding to the range of indexes to return in a list for those
-        particular pagination parameters. """
+    """ The function should return a tuple of size two containing a start index and 
+    an end index corresponding to the range of indexes to return in a list 
+    for those particular pagination parameters. """
     if page and page_size:
         start_index = (page - 1) * page_size
         end_index = start_index + page_size
