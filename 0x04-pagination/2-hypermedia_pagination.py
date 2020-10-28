45cbebfe6ec6a6a54005e17b8001cc85f958d230
raudor <rivaspaulino@outlook.com>
2020-10-28 16:05:19 -0700

    task 2

--- 0x04-pagination/2-hypermedia_pagination.py
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-""" Simple pagination """
+""" Hypermedia pagination """
 import csv
 import math
-from typing import List
+from typing import List, Dict, Any
 
 
 def index_range(page, page_size):
-    """ The function should return a tuple of size two containing a start index and 
-    an end index corresponding to the range of indexes to return in a list 
-    for those particular pagination parameters. """
+    """ return a tuple of size two containing a start index and an end index
+        corresponding to the range of indexes to return in a list for those
+        particular pagination parameters. """
     if page and page_size:
         start_index = (page - 1) * page_size
         end_index = start_index + page_size
